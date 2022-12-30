9d90ac39e73f8f1ed8dc48bd0f402f3240674b43
Thavish Perera <thavish.perera@gmail.com>
2022-12-29 19:52:18 -0500

    Initial Commit

---
+++ KubeViewerApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KubeViewerApp
+//
+//  Created by Thavish Perera on 2022-12-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KubeViewerApp/KubeViewerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KubeViewerAppApp.swift
+//  KubeViewerApp
+//
+//  Created by Thavish Perera on 2022-12-29.
+//
+
+import SwiftUI
+
+@main
+struct KubeViewerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
