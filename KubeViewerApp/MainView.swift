b03a1351c3bd5637ed86763c823b3f259ec84862
Praveen Perera <praveen@avencera.com>
2023-01-29 17:44:52 -0600

    Sidebar button test

--- KubeViewerApp/MainView.swift
+++ KubeViewerApp/MainView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct SideBarButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .foregroundColor(.white)
+            .clipShape(Rectangle())
+    }
+}
+
 struct MainView: View {
     @StateObject private var model = MainViewModel()
     
@@ -16,29 +26,28 @@ struct MainView: View {
                 HStack(spacing: 0) {
                     List {
                         ForEach(model.tabs.values.sorted(by: { s1, s2 in s1.name < s2.name })) { tab in
-                            Button {
-                                model.selectedTab = tab.id
-                            }
-                            label: {
+                            Button(action: {model.selectedTab = tab.id} ) {
                                 HStack {
                                     Text(">")
                                     Text(tab.name)
                                 }
                             }
-                            .buttonStyle(.borderless)
+                            .frame(width: geo.frame(in: .local).width)
+                            .background(Color(red: 0, green: 0, blue: 0.5))
+                            .buttonStyle(SideBarButton())
                         }
-                        .listStyle(.sidebar)
-                    }.navigationTitle(model.tabs[model.selectedTab]!.name)
+                    }.listStyle(.sidebar)
+                        .navigationTitle(model.tabs[model.selectedTab]!.name)
                         .frame(width: geo.size.width * (1/6))
                     
                     model.tabs[model.selectedTab]!.content.padding(10)
                     
-                    Spacer()
-                }
+                    Spacer()}
             }
         }
     }
 }
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
