36d5cf189a040888f4430254684224ea38d7d59b
Praveen Perera <praveen@avencera.com>
2023-02-09 15:45:43 -0600

    Add collapsable list views

--- KubeViewerApp/MainView.swift
+++ KubeViewerApp/MainView.swift
@@ -19,6 +19,7 @@ struct MainView: View {
     @StateObject private var model: MainViewModel = .init()
     @State private var hoverRow: UUID?
     @State private var customSideBarWidth: CGFloat?
+    @State private var expanded: Bool = true
 
     func sidebarWidth(_ geo: GeometryProxy) -> CGFloat {
         max(0, customSideBarWidth ?? max(150, geo.size.width * (1 / 8)))
@@ -29,34 +30,25 @@ struct MainView: View {
             GeometryReader { _ in
                 NavigationSplitView(
                     sidebar: {
-                        ForEach(model.tabs.values.sorted(by: { s1, s2 in s1.name < s2.name })) { tab in
-                            Button(action: { model.selectedTab = tab.id }) {
-                                HStack {
-                                    Text(">")
-                                    Text(tab.name)
+                        VStack {
+                            DisclosureGroup(isExpanded: $expanded, content: {
+                                VStack {
+                                    SidebarButton(text: "Cluster", imageName: "steeringwheel")
+                                    SidebarButton(text: "Nodes", imageName: "server.rack")
                                 }
-                            }
-                            .onTapGesture {
-                                model.selectedTab = tab.id
-                            }
-                            .onHover { hovering in
-                                hoverRow = hovering ? tab.id : nil
-                                DispatchQueue.main.async {
-                                    if hovering {
-                                        NSCursor.pointingHand.push()
-                                    } else {
-                                        NSCursor.pop()
-                                    }
-                                }
-                            }
+                                .padding([.top, .leading], 5)
+                            }, label: {
+                                SidebarTitle()
+                                
+                            })
+                            Spacer()
                         }
+                        .padding(.leading, 10)
                         .navigationTitle(model.tabs[model.selectedTab]!.name)
                     },
-
                     detail: { model.tabs[model.selectedTab]!.content })
             }
-        }.background(WindowAccessor(window: $model.window))
-            .background(BlurWindow())
+        }.background(WindowAccessor(window: $model.window).background(BlurWindow()))
     }
 
     func tabBackgroundColor(_ tab: SideBarTab) -> Color {
@@ -121,5 +113,40 @@ struct ResizerBar: View {
 struct Previews_MainView_Previews: PreviewProvider {
     static var previews: some View {
         /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct SidebarButton: View {
+    var text: String
+    var imageName: String
+
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Label {
+                    Text(text)
+                } icon: {
+                    Image(systemName: imageName)
+                        .foregroundColor(Color.blue)
+                }
+            }.buttonStyle(.plain).padding(.leading, 10)
+            Spacer()
+        }
+        .padding([.top, .bottom], 5)
+        .background(Color.secondary.opacity(0.25))
+        .background(.ultraThinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 4))
+        .frame(maxWidth: .infinity)
+        .padding(.trailing, 15)
+    }
+}
+
+struct SidebarTitle: View {
+    var body: some View {
+        HStack {
+            Text("General")
+                .foregroundColor(.secondary)
+                .font(.system(size: 11, weight: .semibold))
+        }
     }
 }
