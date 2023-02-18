dcb06e2fcfdd0d94d6917207418ffaa5f9ac5070
Praveen Perera <praveen@avencera.com>
2023-02-18 17:14:00 -0600

    Update build.sh

--- bindings/build.sh
+++ bindings/build.sh
@@ -3,12 +3,12 @@
 [ -z "$1" ] && BUILD_TYPE=debug
 [ -z "$1" ] || BUILD_TYPE=$1
 
-echo "BUILD TYPE $BUILD_TYPE"
-
-../xc-universal-binary.sh libkube_viewer.a kube_viewer "${PWD}" "$BUILD_TYPE"
-
+echo "generating bindings from udl file (${BUILD_TYPE})"
 cargo run --features=cli --bin uniffi-bindgen \
     generate src/kube_viewer.udl \
     --language swift \
     --out-dir src/generated \
     --lib-file target/aarch64-apple-darwin/debug/libkube_viewer.a
+
+echo "building universal binary (${BUILD_TYPE})"
+../xc-universal-binary.sh libkube_viewer.a kube_viewer "${PWD}" "$BUILD_TYPE"
