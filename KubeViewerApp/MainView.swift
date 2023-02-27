4d19cc5adda2dfb4606018c1352c3c18295e5203
Praveen Perera <praveen@avencera.com>
2023-02-26 21:46:33 -0600

    Implement scrolling to focus region

--- KubeViewerApp/MainView.swift
+++ KubeViewerApp/MainView.swift
@@ -7,8 +7,12 @@
 import SwiftUI
 
 struct MainView: View {
-    @StateObject private var model: MainViewModel = .init()
+    struct ScrollId: Hashable, Equatable {
+        var tabGroupId: TabGroupId
+        var tabId: TabId? = nil
+    }
 
+    @StateObject private var model: MainViewModel = .init()
     @State private var hoverRow: UUID?
     @State private var expanded: Bool = true
     @State private var search: String = ""
@@ -19,42 +23,58 @@ struct MainView: View {
                 NavigationSplitView(
                     sidebar: {
                         VStack {
-                            ScrollView {
-                                SearchBar(text: $search)
-                                    .padding(.top, 15)
-                                    .padding(.vertical, 10)
-                                    .padding(.horizontal, 12)
+                            ScrollViewReader { proxy in
+                                ScrollView {
+                                    SearchBar(text: $search)
+                                        .padding(.top, 15)
+                                        .padding(.vertical, 10)
+                                        .padding(.horizontal, 12)
 
-                                ForEach(model.data.tabGroupsFiltered(search: search)) { tabGroup in
-                                    DisclosureGroup(isExpanded: $model.tabGroupExpansions[tabGroup.id] ?? true) {
-                                        VStack {
-                                            ForEach(tabGroup.tabs) { tab in
-                                                SidebarButton(tab: tab, selectedTab: $model.selectedTab)
+                                    ForEach(model.data.tabGroupsFiltered(search: search)) { tabGroup in
+                                        DisclosureGroup(isExpanded: $model.tabGroupExpansions[tabGroup.id] ?? true) {
+                                            VStack {
+                                                ForEach(tabGroup.tabs) { tab in
+                                                    SidebarButton(tab: tab, selectedTab: $model.selectedTab)
+                                                        .id(ScrollId(tabGroupId: tabGroup.id, tabId: tab.id))
+                                                }
+                                            }
+                                            .padding(.leading, 5)
+                                        } label: {
+                                            SidebarTitle(name: tabGroup.name)
+                                        }
+                                        .disclosureGroupStyle(SidebarDisclosureGroupStyle())
+                                        .padding(.vertical, 10)
+                                        .padding(.horizontal, 12)
+                                        .overlay {
+                                            switch model.currentFocusRegion {
+                                                case let .sidebarGroup(id: id) where id == tabGroup.id,
+                                                     let .inTabGroup(tabGroupId: id, tabId: _) where id == tabGroup.id:
+                                                    StandardFocusRing()
+                                                default:
+                                                    Color.clear
                                             }
                                         }
-                                        .padding(.leading, 5)
-                                    } label: {
-                                        SidebarTitle(name: tabGroup.name)
+                                        .id(ScrollId(tabGroupId: tabGroup.id))
+                                        .onReceive(model.$currentFocusRegion, perform: { currentFocusRegion in
+                                            withAnimation(.easeIn) {
+                                                switch currentFocusRegion {
+                                                    case let .sidebarGroup(id: tabGroupId):
+                                                        proxy.scrollTo(ScrollId(tabGroupId: tabGroupId))
+                                                    case let .inTabGroup(tabGroupId: tabGroupId, tabId: tabId):
+                                                        proxy.scrollTo(ScrollId(tabGroupId: tabGroupId, tabId: tabId))
+                                                    default:
+                                                        ()
+                                                }
+                                            }
+                                        })
                                     }
-                                    .disclosureGroupStyle(SidebarDisclosureGroupStyle())
-                                    .padding(.vertical, 10)
-                                    .padding(.horizontal, 12)
-                                    .overlay {
-                                        switch model.currentFocusRegion {
-                                            case let .sidebarGroup(id: id) where id == tabGroup.id,
-                                                 let .inTabGroup(tabGroupId: id, tabId: _) where id == tabGroup.id:
-                                                StandardFocusRing()
-                                            default:
-                                                Color.clear
-                                        }
-                                    }
-                                }
 
-                                Spacer()
+                                    Spacer()
+                                }
+                                .navigationTitle(model.tabsMap[model.selectedTab]?.name ?? "Unknown tab")
+                                .padding(.top, 5)
+                                .padding(.horizontal, 8)
                             }
-                            .navigationTitle(model.tabsMap[model.selectedTab]?.name ?? "Unknown tab")
-                            .padding(.top, 5)
-                            .padding(.horizontal, 8)
                         }
 
                         Divider()
