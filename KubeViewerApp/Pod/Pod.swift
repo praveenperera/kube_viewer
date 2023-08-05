974f802323e78203ed83d8d23ec9a17da49e0091
Praveen Perera <praveen@avencera.com>
2023-08-04 23:07:58 -0400

    Use protocols to extract out `age()` and `createdAtTimestamp()`

--- KubeViewerApp/Pod/Pod.swift
+++ KubeViewerApp/Pod/Pod.swift
@@ -7,27 +7,4 @@
 
 import Foundation
 
-extension Pod: Identifiable {
-    func age() -> String? {
-        guard let timestamp = self.createdAt else {
-            return nil
-        }
-
-        let createdAt = Date(timeIntervalSince1970: Double(timestamp))
-
-        let formatter = RelativeDateTimeFormatter()
-        formatter.unitsStyle = .full
-
-        return formatter.localizedString(for: createdAt, relativeTo: Date())
-    }
-
-    func createdAtTimestamp() -> String? {
-        guard let timestamp = self.createdAt else {
-            return nil
-        }
-
-        let createdAt = Date(timeIntervalSince1970: Double(timestamp))
-
-        return createdAt.formatted()
-    }
-}
+extension Pod: Identifiable, CreatedAt, AgeTimestamp {}
