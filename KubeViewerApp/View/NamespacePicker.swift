2886d9cb73eb5cb926ea772af3a77a744929519e
Praveen Perera <praveen@avencera.com>
2023-09-29 17:29:23 -0400

    Create NamespacePicker with ability to callback on change

--- KubeViewerApp/View/NamespacePicker.swift
+++ KubeViewerApp/View/NamespacePicker.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct NamespacePicker: View {
+    let namespaces: [String]
+    @State var selected: String
+
+    init(namespaces: [String], selected: String) {
+        // add "All" option
+        var namespaces = namespaces
+        namespaces.insert("All", at: 0)
+
+        self.namespaces = namespaces
+        self.selected = selected
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Picker("", selection: $selected) {
+            ForEach(namespaces, id: \.self) { namespace in
+                Text(namespace)
+            }
+        }
+        .pickerStyle(.menu)
+        .onChange(of: selected) {
+            // #TODO: send to selected namespace to model
+        }
     }
 }
 
-#Preview {
-    NamespacePicker()
+#Preview("NamespacePicker") {
+    let namespaces = ["default", "devops", "praveen", "growth", "marketing"]
+
+    let view = HStack {
+        NamespacePicker(namespaces: namespaces, selected: "All")
+            .padding()
+    }
+    .frame(minWidth: 250, minHeight: 250)
+
+    return view
 }
